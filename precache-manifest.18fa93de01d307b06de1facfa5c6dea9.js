450a4157345f14ff7ae8eeb0840fbb65aa8ec158
sathvikpurushotham <sathvikpurushotham@gmail.com>
2020-08-31 00:30:54 +0530

    Updates

---
+++ precache-manifest.18fa93de01d307b06de1facfa5c6dea9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3eb56608ae4585a67f3e97a94273806a",
+    "url": "/projectplanner/index.html"
+  },
+  {
+    "revision": "178d76582a33f3fe84ac",
+    "url": "/projectplanner/static/css/main.5938a27e.chunk.css"
+  },
+  {
+    "revision": "bbbdc558572b3990e2ca",
+    "url": "/projectplanner/static/js/2.1190ffd7.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/projectplanner/static/js/2.1190ffd7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "178d76582a33f3fe84ac",
+    "url": "/projectplanner/static/js/main.e582be7e.chunk.js"
+  },
+  {
+    "revision": "fb3bb6050cee0d519b01",
+    "url": "/projectplanner/static/js/runtime-main.6a492152.js"
+  }
+]);
